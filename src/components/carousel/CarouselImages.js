39d222d498a670f4016193d21277bd7878f6cbb3
MircoFiocchiSW <mirco.fiocchi@southworks.com>
2022-01-01 12:30:05 -0300

    [carousel] - update images

---
+++ src/components/carousel/CarouselImages.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Carousel } from 'react-carousel-minimal';
+import data from './data/data';
+
+const CarouselImages = () => {
+  const captionStyle = {
+    fontSize: '2em',
+    fontWeight: 'bold',
+  };
+  return (
+      <Carousel
+        data={data}
+        time={3000}
+        width="100%"
+        height="500px"
+        captionStyle={captionStyle}
+        captionPosition="bottom"
+        automatic={true}
+        dots={true}
+        pauseIconColor="white"
+        pauseIconSize="40px"
+        slideBackgroundColor="darkgrey"
+        slideImageFit="cover"
+        thumbnailWidth="100px"
+      />
+  );
+};
+
+export default CarouselImages;
---
+++ src/components/carousel/data/data.js
@@ -0,0 +1,15 @@
+import cartelPatiecito from '../images/cartelPatiecito.png';
+import barDeAmigos from '../images/barDeAmigos.png'
+
+const data = [
+  {
+    image: cartelPatiecito,
+    caption: 'Patiecito 2022',
+  },
+  {
+    image: barDeAmigos,
+    caption: 'Bar de amigxs',
+  },
+];
+
+export default data;
